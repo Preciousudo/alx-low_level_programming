27f796aabe1ca4e4673bfc0ae74abb2035026540
Preciousudo <udoprecious226@gmail.com>
2022-12-12 16:58:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
- */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        puts("\"Programming is like building a multi lingual puzzle);                                                            
-        return (0);
+#include <stdio.h>
+
+/**
+ * main - print a given phrase
+ * Rerurn: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
 }
