a381508656cf167c0e7ceddc4d3f2bd5fa64b5ae
Preciousudo <udoprecious226@gmail.com>
2022-12-12 16:27:48 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a given phrase
+ * Return: O if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multifunctional puzzle");
+	Return (0);
+}
